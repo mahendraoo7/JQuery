f96e8752f6b7c0b4f04e8e64eed54af181563480
Mahendra <mahendrapurohit9889@gmail.com>
2024-09-30 11:08:54 +0530

    Jquery

--- jQuery.js
+++ jQuery.js
@@ -18,3 +18,108 @@ $(function(){
     });
   });
 })})
+
+
+// $(document).ready(function(){
+
+//     // jQuery methods go here...
+  
+//   });
+
+// jQuery Selectors
+
+// Example
+// $(document).ready(function(){
+//   $("button").click(function(){
+//     $("p").hide();
+//   });
+// });
+
+// $("*")	Selects all elements	
+// $(this)	Selects the current HTML element	
+// $("p.intro")	Selects all <p> elements with class="intro"	
+// $("p:first")	Selects the first <p> element	
+// $("ul li:first")	Selects the first <li> element of the first <ul>	
+// $("ul li:first-child")	Selects the first <li> element of every <ul>	
+// $("[href]")	Selects all elements with an href attribute	
+// $("a[target='_blank']")	Selects all <a> elements with a target attribute value equal to "_blank"	
+// $("a[target!='_blank']")	Selects all <a> elements with a target attribute value NOT equal to "_blank"	
+// $(":button")	Selects all <button> elements and <input> elements of type="button"	
+// $("tr:even")	Selects all even <tr> elements	
+// $("tr:odd")	Selects all odd <tr> elements
+
+
+
+/*  jQuery Get Html Method */
+
+/*
+text()
+html()
+val()
+*/
+
+// $(function(){
+//     $('#btn').click(function(){
+//         console.log($('#heading').text())
+//     })
+// })
+
+
+// $(function(){
+//     $('#btn').click(function(){
+//         console.log($('#heading').html())
+//     })
+// })
+
+// $(function(){
+//     $('#btn').click(function(){
+//         console.log($('#inputs').val())
+//     })
+// })
+
+/*  jQuery Set Html Method */
+
+// $(function(){
+//     $('#btn').click(function(){
+//         $('#heading').text('<span>This is Heading Tag</span>')
+//     })
+// })
+
+// $(function(){
+//     $('#btn').click(function(){
+//         $('#heading').html('<span>This is Heading Tag</span>')
+//     })
+// })
+
+// $(function(){
+//     $('#btn').click(function(){
+//         $('#inputs').val('This is Heading Tag')
+//     })
+// })
+
+/* Jquery Add Method */
+
+
+// $(function(){
+//     $('#btn').dblclick(function(){
+//         $('#heading').append('<span>Elements</span>')
+//     })
+// })
+
+// $(function(){
+//     $('#btn').dblclick(function(){
+//         $('#heading').prepend('<span>Elements</span>')
+//     })
+// })
+
+// $(function(){
+//     $('#btn').dblclick(function(){
+//         $('#heading').after('<span>Elements</span>')
+//     })
+// })
+
+// $(function(){
+//     $('#btn').dblclick(function(){
+//         $('#heading').before('<span>Elements</span>')
+//     })
+// })
