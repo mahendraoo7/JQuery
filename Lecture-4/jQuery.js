988db409c0e3e7298179425d7a83020686b8a84b
Mahendra <mahendrapurohit9889@gmail.com>
2024-10-01 11:45:30 +0530

    JQuery

---
+++ Lecture-4/jQuery.js
@@ -0,0 +1,37 @@
+/*  jQuery Remove Method */
+
+// $(function(){
+//     $("#btn").click(function(){
+//         $('#text').remove()
+//     })
+// })
+
+// $(function(){
+//     $("#btn").click(function(){
+//         $('#text').empty()
+//     })
+// })
+
+/* jQuery addClass , removeClass And toggleClass Method */
+
+// $(function(){
+//     $('#btn').click(function(){
+//         $('#demo').addClass('box')
+//     })
+// })
+
+// $(function(){
+//     $('#btn').click(function(){
+//         $('#demo').removeClass('box')
+//     })
+// })
+
+// $(function(){
+//     $('#btn').click(function(){
+//         $('#demo').toggleClass('box')
+//     })
+// })
+
+/* jQuery Traversing */
+
+
