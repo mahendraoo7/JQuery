2f07d16ba810c5ae5dbe11c65f9864b6faf69976
Mahendra <mahendrapurohit9889@gmail.com>
2024-10-18 18:35:49 +0530

    Query

--- Lecture-6/Jquery.js
+++ Lecture-6/Jquery.js
@@ -1,24 +1,33 @@
-$(function(){
-    $('#btn').click(function() {
-        $('div').children().css({
-            backgroundColor : 'yellow'
-        })
-    })
-})
+// $(function(){
+//     $('#btn').click(function() {
+//         $('div').children().css({
+//             backgroundColor : 'yellow'
+//         })
+//     })
+// })
 
-$(function() { 
-          $('#btn').click(function(){
-              $('div').siblings().css({
-                  backgroundColor : 'yellow'
-              })
-          })
-      })
+// $(function() { 
+//           $('#btn').click(function(){
+//               $('div').siblings().css({
+//                   backgroundColor : 'yellow'
+//               })
+//           })
+//       })
 
-$(function() {
-    $('#btn').click(function() {
-        $('ul').children().css({
-            color : 'red',
-            backgroundColor : 'black'
-        })
-    })
-})
+// $(function() {
+//     $('#btn').click(function() {
+//         $('ul').children().css({
+//             color : 'red',
+//             backgroundColor : 'black'
+//         })
+//     })
+// })
+
+// $(function() {
+//     $('#btn').click(function() {
+//         $('div').eq(1).css({
+//             color : 'red',
+//             backgroundColor : 'black'
+//         })
+//     })
+// })
