6a24e06b708ba7c01f7395acdfb5b0bd0b0d4b84
Mahendra <mahendrapurohit9889@gmai.com>
2024-10-24 18:48:50 +0530

    OnEnter

--- Exercise/Exe15/script.js
+++ Exercise/Exe15/script.js
@@ -1,4 +1,5 @@
 $(function () {
+
     $('#btn').click(function () {
         let world = $('input').val().trim();
         if (world) {
@@ -13,4 +14,21 @@ $(function () {
     $('ul').on('click', 'li', function() {
         $(this).remove();
     });
+
+    $('input').keypress(function(e) {
+        var keycode = e.keyCode || e.which;
+
+        if(keycode == '13') {
+            
+            let world = $('input').val().trim();
+               if (world) {
+                   $('ul').append(`<li>${world}</li>`);
+                   $('input').val('');
+               } else {
+                   alert("Please enter a value.");
+               }  
+
+        }
+    });
 });
+
